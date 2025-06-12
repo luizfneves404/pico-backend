25ef9de44dcfb5c1ba1a29e4afc6a893a24b7d2d
Luiz Felipe Neves Batista <luiz@sophinity.co>
2025-06-12 06:26:13 -0300

    changed docker entrypoint to run alembic too

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,14 +4,8 @@ set -e
 
 . /code/.venv/bin/activate
 
-# Creating database tables, if they don't exist
 echo "Migrating..."
-PYTHONPATH=./pico_django python -m pico_django.manage migrate --noinput
+alembic upgrade head
 echo "Migrations complete"
-
-# Collecting static files
-echo "Collecting static files..."
-PYTHONPATH=./pico_django python -m pico_django.manage collectstatic --noinput
-echo "Static files collected"
 
 exec python -m app.main
